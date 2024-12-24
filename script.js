9fe4c11d6b640fda1eedf77328e8f250634c2596
rank-master <webtoonviralhit@gmail.com>
2024-12-23 18:21:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,3 @@ function showSection(sectionId) {
   sections.forEach(section => section.classList.remove('active'));
   document.getElementById(sectionId).classList.add('active');
 }
-
-function openArticle(title, content) {
-  document.getElementById('article-title').innerText = title;
-  document.getElementById('article-content').innerText = content;
-  document.getElementById('article-modal').style.display = 'flex';
-}
-
-function closeArticle() {
-  document.getElementById('article-modal').style.display = 'none';
-}
