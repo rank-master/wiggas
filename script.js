adb30aa0cae1998e660c4241f4b7918710943711
rank-master <webtoonviralhit@gmail.com>
2024-12-26 07:51:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function showSection(sectionId) {
     sections.forEach(section => section.classList.remove('active'));
     document.getElementById(sectionId).classList.add('active');
   }
+
+document.querySelector(".game-section #roll-dice").addEventListener("click", () => {
+  const dice = Math.floor(Math.random() * 6) + 1;
+  document.querySelector(".game-section #dice-result").innerText = `Dice: ${dice}`;
+});
