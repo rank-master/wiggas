4ddda5c7ec624305882a90053f1b410b6677a774
rank-master <webtoonviralhit@gmail.com>
2025-02-28 19:33:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-    function scrollToSection(sectionId) {
+function scrollToSection(sectionId) {
         const section = document.getElementById(sectionId);
         if (section) {
             section.scrollIntoView({ behavior: 'smooth' });
         }
+}
+
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    if (link.getAttribute('href') === window.location.pathname) {
+        link.classList.add('active');
     }
+});
